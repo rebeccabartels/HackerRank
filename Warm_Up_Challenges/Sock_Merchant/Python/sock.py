f98e9e0aca949cf959a57fd21265ca89c4645c9f
riley <rebeccakbartels@gmail.com>
2019-12-28 12:14:17 -0800

    la dee dah

--- Warm_Up_Challenges/Sock_Merchant/Python/sock.py
+++ Warm_Up_Challenges/Sock_Merchant/Python/sock.py
@@ -8,17 +8,10 @@ import sys
 
 
 def sockMerchant(n, ar):
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = sockMerchant(n, ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    if __name__ == '__main__':
+        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+        n = int(input())
+        ar = list(map(int, input().rstrip().split()))
+        result = sockMerchant(n, ar)
+        fptr.write(str(result) + '\n')
+        fptr.close()
